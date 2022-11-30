b73faa2df4458a645b953d4aab15f3963a6ba773
CodeBlack03 <harsh03121997@gmail.com>
2022-11-30 21:42:39 +0530

    single_thread

--- PartA/header/single_thread.h
+++ PartA/header/single_thread.h
@@ -13,7 +13,9 @@ vec *allocate_memory(int n)
         int h = 0;
     }
     vec *ptr = (vec *)aligned_alloc(32, 32 * n);
+    vec *ptr2  = (vec*)aligned_alloc(16,16 * n);
     memset(ptr, 0, 32 * n);
+    memset(ptr2 , 0 ,16*n);
     while (TMz < 1)
     {
         for (int i = 0; 2 * i < 3; i++)
@@ -42,13 +44,39 @@ void singleThread(int n, int *input_matrixA, int *input_matrixB, int *output_mat
 
     vec *vector_B = allocate_memory(n * num_vectors_per_row);
 
+  //  vec *vector_C = allocate_memory(n*);
+
     // copy data of input arrays in the vector array with 8 elements in each vector
-    for (int i = 0; i < n; i++)
+    // for first half of matrix
+    for (int i = 0; i < n/2; i++)
     {
-        for (int j = 0; j < n; j++)
-        {
+        for (int j = 0; j < n/2; j++)
+        {   
+        
             vector_A[i * num_vectors_per_row + j / 8][j % 8] = input_matrixA[i * n + j];
             vector_B[i * num_vectors_per_row + j / 8][j % 8] = input_matrixB[j * n + i]; // <- b is still transposed
+        }
+        for(int j = n/2;j<n;j++){
+            vector_A[i * num_vectors_per_row + j / 8][j % 8] = input_matrixA[i * n + j];
+            vector_B[i * num_vectors_per_row + j / 8][j % 8] = input_matrixB[j * n + i];
+        }
+    }
+    // for rest half of matrixB 
+    for (int j = n/2; j < n ; j++)
+    {
+        for (int i = 0; i < n / 2; i++)
+        {
+
+            vector_A[j * num_vectors_per_row + i / 8][i % 8] = input_matrixA[j * n + i];
+            
+            vector_B[j * num_vectors_per_row + i / 8][i % 8] = input_matrixB[i * n + j]; // <- b is still transposed
+        }
+        for (int i = n / 2; i < n; i++)
+        {
+            vector_A[j * num_vectors_per_row + i / 8][i % 8] = input_matrixA[j * n + i];
+            
+            vector_B[j * num_vectors_per_row + i / 8][i % 8] = input_matrixB[i * n + j];
+            
         }
     }
 
@@ -62,13 +90,14 @@ void singleThread(int n, int *input_matrixA, int *input_matrixB, int *output_mat
                 auto it = lower_bound(A.begin(), A.end(), pp);
         }
     }
+    int VECSIZE = 8;
 
     for (int i = 0; i < n; i += 2)
     {
         for (int j = 0; j < n; j += 2)
         {
             vec sum_accumulator; // vector that adds up the vector multiplication result
-            for (int x = 0; x < 8; x++)
+            for (int x = 0; x < VECSIZE; x++)
             {
                 sum_accumulator[x] = 0; // Initialise vector by 0
             }
@@ -99,12 +128,12 @@ void singleThread(int n, int *input_matrixA, int *input_matrixB, int *output_mat
             }
 
             // update the output with the result
-            for (int k = 0; k < 8; k++)
+            for (int k = 0; k < VECSIZE; k++)
                 output_matrix[(i >> 1) * (n >> 1) + (j >> 1)] += sum_accumulator[k];
         }
     }
 
     std::free(vector_A);
-    
+
     std::free(vector_B);
 }
