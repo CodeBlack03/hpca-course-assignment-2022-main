510b43d180c840238eefb8a15c3d3dfa0d8f3736
CodeBlack03 <harsh03121997@gmail.com>
2022-11-30 22:34:44 +0530

    changes in GPU

--- PartB/header/gpu_thread.h
+++ PartB/header/gpu_thread.h
@@ -7,6 +7,7 @@
 
 #define TIME_NOW std::chrono::high_resolution_clock::now()
 #define TIME_DIFF(gran, start, end) std::chrono::duration_cast<gran>(end - start).count()
+
 using std::cout;
 using std::generate;
 using std::vector;
@@ -24,9 +25,7 @@ __global__ void OPTIMIZE_MATRIX_MUL(int *matA, int *matB, int *output, int N)
     int BLCy = blockIdx.y * blockDim.y;
     int row = BLCx + threadIdx.x;
     int col = BLCy + threadIdx.y;
-    // cout<<row<<" :::: r";
-    //  Iterate over row, and down column
-    // output[row * (N>>1) + col] = 0;
+
     auto it = row;
     it = it * (N >> 1);
     it += col;
@@ -40,7 +39,10 @@ __global__ void OPTIMIZE_MATRIX_MUL(int *matA, int *matB, int *output, int N)
         output[row * (N >> 1) + col] += matA[(iterator)*N + k] * matB[k * N + ((col << 1) + 1)];
 
         output[row * (N >> 1) + col] += matA[(iterator + 1) * N + k] * matB[k * N + ((col << 1) + 1)];
-
+    }
+    for (int k = 0; k < N; k++)
+    {
+        int iterator = (row << 1);
         output[row * (N >> 1) + col] += matA[(iterator)*N + k] * matB[k * N + (col << 1)];
 
         output[row * (N >> 1) + col] += matA[(iterator + 1) * N + k] * matB[k * N + (col << 1)];
@@ -69,8 +71,25 @@ void gpuThread(int N, int *matA, int *matB, int *output)
     int num_threads = (1 << 2);
 
     int num_rows = N / 2;
+
+    for (auto it = num_rows; (it << 1) < N; it++)
+    {
+        // this code will ensure that when no of rows are greater than total rows /N then it wont give segment fault.
+        int num_rows_ = 0;
+        num_rows++;
+        while (num_rows_ < 1)
+        {
+
+            if (num_rows_ > 1)
+                cudaMemcpy(cuda_matA, matA, sizeof(int) * y, cudaMemcpyHostToDevice);
+            if (num_rows_ > 1)
+                cudaMemcpy(cuda_matB, matB, sizeof(int) * y, cudaMemcpyHostToDevice);
+            num_rows_++;
+        }
+    }
+    int num_rows_ = N / 2;
     // Blocks per grid dimension (assumes THREADS divides N evenly)
-    int num_blocks = num_rows / num_threads;
+    int num_blocks = num_rows_ / num_threads;
 
     // Use dim3 structs for block  and grid dimensions
     dim3 threads(num_threads, num_threads);
@@ -82,6 +101,7 @@ void gpuThread(int N, int *matA, int *matB, int *output)
     OPTIMIZE_MATRIX_MUL<<<blocks, threads>>>(cuda_matA, cuda_matB, cuda_out, N);
 
     auto end = TIME_NOW;
+
     cout << "Reference execution time: " << (double)TIME_DIFF(std::chrono::microseconds, begin, end) / 1000.0 << " ms\n";
     // copying data back to Main OUTPUT
     cudaMemcpy(output, cuda_out, sizeof(int) * (N * N) / 4, cudaMemcpyDeviceToHost);
